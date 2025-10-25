bcba966390ff4af78d6a8dd3bd3d2bbe0aed8a60
MakushevEvgenii <evmakush@gmail.com>
2025-10-25 19:55:16 +0300

    25.10

---
+++ src/Directions.java
@@ -0,0 +1,14 @@
+public enum Directions{
+    INPUT("src/resources/"),//путь к директории вводных данных
+    ARCHIVE("src/resources/");//путь к директории архива
+
+    private final String path;
+
+    Directions(String path) {
+        this.path = path;
+    }
+
+    public String getPath() {
+        return path;
+    }
+}
---
+++ src/servise/AccountServise.java
@@ -0,0 +1,20 @@
+package servise;
+
+import account.Account;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class AccountServise {
+    private static final Map<String, Account>accountMap=new HashMap<>();
+    static {//отработает один раз
+        accountMap.put("1234567890",new Account("1234567890", 5000));
+        accountMap.put("0987654321",new Account("0987654321", 5000));
+        accountMap.put("1122334455",new Account("1122334455", 5000));
+        accountMap.put("5544332211",new Account("5544332211", 5000));
+        accountMap.put("9876543210",new Account("9876543210", 5000));
+        accountMap.put("4455667788",new Account("4455667788", 5000));
+        accountMap.put("8877665544",new Account("8877665544", 5000));
+
+    }
+}
